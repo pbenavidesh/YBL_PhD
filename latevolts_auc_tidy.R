3f845f6e30917cdaa3cabb23057150c4bf131cb6
pbenavidesh <pbenavides@iteso.mx>
2020-05-29 14:14:22 -0500

    latevolts auc

---
+++ latevolts_auc_tidy.R
@@ -0,0 +1,12 @@
+# Limpieza de datos de latevolts.xlsx
+
+
+# pkgs --------------------------------------------------------------------
+
+library(easypackages)
+libraries("tidyverse")
+
+
+# data import -------------------------------------------------------------
+
+df <- read_cs
